2f155bd74d41a2b57f5580c4356a8d9bfbff45fb
miquelcanyellas <83391629+miquelcanyellas@users.noreply.github.com>
2021-05-27 08:42:29 +0200

    Add files via upload

--- Tenda-forestal-ElTronc-Miguel/includes/header.php
+++ Tenda-forestal-ElTronc-Miguel/includes/header.php
@@ -1,9 +1,27 @@
 <center>
-<a href="index.php"> Index</a>
-<a href="insert_producte.php">Insertar un Producte</a>
-<a href="list_producte.php">Llista de Productes</a>
-<a href="insert_proveidor.php">Insertar un Proveidor</a>
-<a href="list_proveidor.php">Llista de Proveidors</a>
-<a href="insert_client.php">Insertar un Client</a>
-<a href="list_client.php">Llista de Clients</a>
+<nav id="menu">
+
+<ul>
+ <li><a href="index.php">Index</a></li>
+ <li><a href="#">Productes</a>
+
+ <ul>
+ <li><a href="insert_producte.php">Insertar nou producte</a></li>
+ <li><a href="list_producte.php">Llista de productes</a></li>
+ </ul>
+
+ </li>
+ <li><a href="#">Proveïdor</a>
+
+ <ul>
+ <li><a href="insert_proveidor.php">Insertar nou proveïdor</a></li>
+ <li><a href="list_proveidor.php">Llista de proveïdors</a></li>
+ </ul>
+
+ </li>
+ <li><a href="#">Client</a>
+ <ul>
+ <li><a href="insert_client.php">Insertar nou Client</a></li>
+ <li><a href="list_client.php">Llista de Clients</a></li>
+ </ul>
 </center>
